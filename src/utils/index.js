beca772ed071f7776c27e6b1bf377ec6b21e3942
triyys <ngoductri123@gmai.com>
2022-05-26 16:25:57 +0700

    remove navigationDictionary

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,2 @@
 export { default as categoryDictionary } from './categoryDictionary'
-export { default as navigationBarDictionary } from './navigationBarDictionary'
 export { default as formattingNumber } from './numberWithCommas'
